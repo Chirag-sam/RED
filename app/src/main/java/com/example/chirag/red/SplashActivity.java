67caac7e8680e54065d8c2f5c017b10f754ad39a
krsnvijay <krsnvijay@gmail.com>
2017-08-31 20:41:56 +0530

    Splash Screen(Basic)

---
+++ app/src/main/java/com/example/chirag/red/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.chirag.red;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by krsnv on 8/31/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Intent intent = new Intent(this, StartActivity.class);
+    startActivity(intent);
+    finish();
+  }
+}
